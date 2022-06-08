c2ff571c3ba16e476392dfbfdf4ac903c05d609e
Girish Madnani <girishmadnani05@gmail.com>
2022-06-08 09:23:13 +0100

    content to db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,20 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  //connect to db
+  const jateDb = await openDB("jate", 1);
+  //change to read and write mode
+  const tx = jateDb.transaction("jate", "readwrite");
+  //add to store
+  const store = tx.objectStore("jate");
+  await store.put({ id: 0, content: content });
+  console.log("Text saved to the database");
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+
+};
 
 initdb();
