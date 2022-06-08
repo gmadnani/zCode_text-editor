bef605559c07c65424eeda9404bafc3daffd835a
Girish Madnani <girishmadnani05@gmail.com>
2022-06-08 02:15:35 +0100

    cashing for assets

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,16 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+ // Callback function to filter the requests to cache
+ ({ request }) => ["style", "script", "worker"].includes(request.destination),
+ new StaleWhileRevalidate({
+   cacheName: "asset-cache",
+   plugins: [
+     //  Max age of 30 days
+     new CacheableResponsePlugin({
+       statuses: [0, 200],
+     }),
+   ],
+ })
+);
